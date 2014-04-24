625dc97dc89f45933687b51cf05816a16806d82b
Timothy Lee <timothy1ee@yahoo.com>
2014-04-24 14:19:24 -0700

    Initial Commit

---
+++ Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tomatoes
+//
+//  Created by Timothy Lee on 4/24/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
